81be329b6fa4192a7f08b518f93c9b6a6ee8b29e
odevpedro <pedrosschmidt2@gmail.com>
2022-12-23 14:19:17 -0300

    hello world

---
+++ src/nativeMain/kotlin/Main.kt
@@ -0,0 +1,35 @@
+fun main() {
+    println("Hello, Kotlin/Native!")
+
+        // Boolean data type
+        var isTrue: Boolean = true
+        var isFalse: Boolean = false
+
+        // Numeric data types
+        var a: Byte = 127 // 8-bit signed integer
+        var b: Short = 32767 // 16-bit signed integer
+        var c: Int = 2147483647 // 32-bit signed integer
+        var d: Long = 9223372036854775807 // 64-bit signed integer
+        var e: Float = 3.14f // 32-bit floating point number
+        var f: Double = 3.14159265358979323846 // 64-bit floating point number
+
+        // Char data type
+        var g: Char = 'A' // Single Unicode character
+
+        // String data type
+        var h: String = "Hello, world!" // Sequence of characters
+
+
+    // Use the variables
+    println(isTrue)
+    println(isFalse)
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+    println(g)
+    println(h)
+}
+
