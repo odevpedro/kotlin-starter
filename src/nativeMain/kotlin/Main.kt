79d8c82592ec70ff73dcff361e2dd3cf37635cb3
odevpedro <pedrosschmidt2@gmail.com>
2022-12-27 17:23:04 -0300

    flux control

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,17 +1,51 @@
-fun calculeBonification(a: Int, b: Int, c: Int)  {
-    println("The bonus is:  ${a + b + c}")
-}
-
-fun hello(name: String) : String {
-    return "ola, $name"
-}
-
-//one line function
-fun hello2(name: String) = "Hello, $name"
-
 fun main() {
+    val x = 10
 
-calculeBonification(10, 20, 20)
-    hello("hi")
+    // if-else statement
+    if (x > 0) {
+        println("x is positive")
+    } else {
+        println("x is negative or zero")
+    }
+
+    // when expression
+    when (x) {
+        in 1..10 -> println("x is in the range 1 to 10")
+        !in 11..20 -> println("x is not in the range 11 to 20")
+        else -> println("x is outside the range 1 to 20")
+    }
+
+    // for loop
+    for (i in 1..5) {
+        println(i)
+    }
+
+    // while loop
+    var y = 10
+    while (y > 0) {
+        println(y)
+        y--
+    }
+
+    // do-while loop
+    do {
+        println(y)
+        y++
+    } while (y < 10)
+
+
+    //elvis operator
+    val test: Int? = null
+    val op: Int = test ?: 100
+    print(op) //return 100
+
+
+    //elvis operator
+    /*
+    var test: Int? = null
+    test = 10
+    val op: Int = test ?: 100
+
+    print(op) //rrturn 10
+*/
 }
-
