e08287722ca54b6c957e56b14220f0054e2ee71a
odevpedro <pedrosschmidt2@gmail.com>
2022-12-27 17:48:50 -0300

    conversion types

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,51 +1,72 @@
 fun main() {
-    val x = 10
+    // String to number conversions
+    val stringInt = "42"
+    val int = stringInt.toInt()
+    println("stringInt = $stringInt, int = $int")
 
-    // if-else statement
-    if (x > 0) {
-        println("x is positive")
-    } else {
-        println("x is negative or zero")
-    }
+    val stringLong = "9001"
+    val long = stringLong.toLong()
+    println("stringLong = $stringLong, long = $long")
 
-    // when expression
-    when (x) {
-        in 1..10 -> println("x is in the range 1 to 10")
-        !in 11..20 -> println("x is not in the range 11 to 20")
-        else -> println("x is outside the range 1 to 20")
-    }
+    val stringFloat = "3.14"
+    val float = stringFloat.toFloat()
+    println("stringFloat = $stringFloat, float = $float")
 
-    // for loop
-    for (i in 1..5) {
-        println(i)
-    }
+    val stringDouble = "2.71828"
+    val double = stringDouble.toDouble()
+    println("stringDouble = $stringDouble, double = $double")
 
-    // while loop
-    var y = 10
-    while (y > 0) {
-        println(y)
-        y--
-    }
-
-    // do-while loop
-    do {
-        println(y)
-        y++
-    } while (y < 10)
-
-
-    //elvis operator
-    val test: Int? = null
-    val op: Int = test ?: 100
-    print(op) //return 100
-
-
-    //elvis operator
     /*
-    var test: Int? = null
-    test = 10
-    val op: Int = test ?: 100
 
-    print(op) //rrturn 10
-*/
+
+       // String to number conversions with nullable result
+    val invalidStringInt = "forty-two"
+    val invalidInt = invalidStringInt.toIntOrNull()
+    println("invalidStringInt = $invalidStringInt, invalidInt = $invalidInt")
+
+    val stringByte = "42"
+    val byte = stringByte.toByte()
+    println("stringByte = $stringByte, byte = $byte")
+
+    val stringShort = "9001"
+    val short = stringShort.toShort()
+    println("stringShort = $stringShort, short = $short")
+
+    val stringChar = "A"
+    val char = stringChar.toChar()
+    println("stringChar = $stringChar, char = $char")
+
+    // Number to string conversions
+    val int = 42
+    val stringInt = int.toString()
+    println("int = $int, stringInt = $stringInt")
+
+    val long = 9001L
+    val stringLong = long.toString()
+    println("long = $long, stringLong = $stringLong")
+
+    val float = 3.14f
+    val stringFloat = float.toString()
+    println("float = $float, stringFloat = $stringFloat")
+
+    val double = 2.71828
+    val stringDouble = double.toString()
+    println("double = $double, stringDouble = $stringDouble")
+
+    val byte: Byte = 42
+    val stringByte = byte.toString()
+    println("byte = $byte, stringByte = $stringByte")
+
+    val short: Short = 9001
+    val stringShort = short.toString()
+    println("short = $short, stringShort = $stringShort")
+
+    val char: Char = 'A'
+    val stringChar = char.toString()
+    println("char = $char, stringChar = $stringChar")
+    *
+    * */
+
+
+
 }
