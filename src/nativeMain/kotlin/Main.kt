09e3a0d0dba539038fd9d9115052cd9a6c041d16
odevpedro <pedrosschmidt2@gmail.com>
2022-12-27 16:14:42 -0300

    fix: basicFunctions

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,45 +1,17 @@
-fun main() {
-    // Arithmetic operations
-    val a = 10
-    val b = 20
-    val sum = a + b
-    val difference = a - b
-    val product = a * b
-    val quotient = a / b
-    val remainder = a % b
-
-    println("Sum: $sum")
-    println("Difference: $difference")
-    println("Product: $product")
-    println("Quotient: $quotient")
-    println("Remainder: $remainder")
-
-    // Comparison operations
-    val c = 30
-    val isEqual = (a == c)
-    val isNotEqual = (a != c)
-    val isGreaterThan = (a > c)
-    val isLessThan = (a < c)
-    val isGreaterThanOrEqual = (a >= c)
-    val isLessThanOrEqual = (a <= c)
-
-    println("a is equal to c: $isEqual")
-    println("a is not equal to c: $isNotEqual")
-    println("a is greater than c: $isGreaterThan")
-    println("a is less than c: $isLessThan")
-    println("a is greater than or equal to c: $isGreaterThanOrEqual")
-    println("a is less than or equal to c: $isLessThanOrEqual")
-
-    // Logical operations
-    val d = true
-    val e = false
-    val f = !d
-    val g = (d && e)
-    val h = (d || e)
-
-    println("d: $d")
-    println("e: $e")
-    println("f (not d): $f")
-    println("g (d and e): $g")
-    println("h (d or e): $h")
+fun calculeBonification(a: Int, b: Int, c: Int)  {
+    println("The bonus is:  ${a + b + c}")
 }
+
+fun hello(name: String) : String {
+    return "ola, $name"
+}
+
+//one line function
+fun hello2(name: String) = "Hello, $name"
+
+fun main() {
+
+calculeBonification(10, 20, 20)
+    hello("hi")
+}
+
